e49d5f4ee9dabdbb422ec40f982bdcea980e4f4f
Matthew Cruz <matthew.j.cruz111@gmail.com>
2024-09-05 20:02:35 -0500

    Figuring Out Fizzbuzz
    This commit contains the current malfunctioning version of FizzBuzzNew and the current working version of FizzBuzz, verbalized for troubleshooting purposes.

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -1,38 +1,33 @@
-import java.io.FileWriter; 
-public class Fizzbuzz
-{
-  public static void main(String args[])
-  {
-    try
-    {
-      FileWriter fw=new FileWriter("C:\\\\Users\\matth\\OneDrive\\Programming OneDrive\\Java-Practice\\Fizzbuzz.txt");
-      for(int x = 1; x <= 100; x++)
-      {
-        if( x % 5 == 0)
-        {
-          fw.write("fizz");
-          if(x % 7 == 0)
-          {
-            fw.write("buzz\n");
-          }
-          else
-          {
-            fw.write("\n");
-          }
+import java.io.FileWriter;
+
+public class Fizzbuzz {
+    public static void main(String args[]) {
+        try {
+            FileWriter fw = new FileWriter("C:\\\\Users\\matth\\OneDrive\\Programming OneDrive\\Java-Practice\\Fizzbuzz.txt");
+            for (int x = 1; x <= 100; x++) {
+                if (x % 5 == 0) {
+                    fw.write("fizz");
+                    System.out.println("fizz");
+                    if (x % 7 == 0) {
+                        fw.write("buzz\n");
+                        System.out.println("buzz\n");
+                    } else {
+                        fw.write("\n");
+                        System.out.println("\n");
+                    }
+                } else if (x % 7 == 0) {
+                    fw.write("Buzz\n");
+                    System.out.println("Buzz\n");
+                } else {
+                    fw.write(x + "\n");
+                    System.out.println("\n");
+                }
+            }
+            fw.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        else if(x % 7 == 0)
-        {
-          fw.write("buzz\n");
-        }
-        else
-        {
-          fw.write(x + "\n");
-        }
-      }
-      fw.close();
-      
+        System.out.println("Success...");
     }
-    catch(Exception e){System.out.println(e);}    
-    System.out.println("Success...");    
-  }
 }
--- FizzbuzzNew.java
+++ FizzbuzzNew.java
@@ -1,35 +1,30 @@
-import java.io.FileWriter; 
-public class FizzbuzzNew
-{
-    public static void main(String args[])
-    {
-       try
-    {
-      FileWriter fw = new FileWriter("C:\\Users\\matth\\OneDrive\\Programming OneDrive\\Java-Practice\\FizzbuzzNew.txt");
-        for(int i = 1;i<=100;i++)
-        {
-            System.out.println(i);
-            fw.write(i);
-            if(i%5==0)
-            {
-                System.out.println(" Fizz");
-                fw.write(" Fizz");
-                if(i%7==0)
-                  {
-                      System.out.println("buzz");
-                      fw.write("buzz");
+import java.io.FileWriter;
+
+public class FizzbuzzNew {
+    public static void main(String args[]) {
+        try {
+            FileWriter fw = new FileWriter("C:\\Users\\matth\\OneDrive\\Programming OneDrive\\Java-Practice\\FizzbuzzNew.txt");
+            for (int i = 1; i <= 100; i++) {
+                System.out.println(i);
+                fw.write(i);
+                if (i % 5 == 0) {
+                    System.out.println(" Fizz");
+                    fw.write(" Fizz");
+                    if (i % 7 == 0) {
+                        System.out.println("buzz");
+                        fw.write("buzz");
+                    }
+                }
+                if (i % 7 == 0) {
+                    fw.write("buzz");
+                    System.out.println("buzz");
+                }
+                fw.write("\n");
+                System.out.println("\n");
             }
-            }
-            if(i%7==0)
-            {
-                fw.write("buzz");
-                System.out.println("buzz");
-            }
-            fw.write("\n");
-            System.out.println("\n");
+            fw.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        fw.close();
     }
-       catch(Exception e){System.out.println(e);} 
-}
 }
